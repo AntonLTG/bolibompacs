9b783ac124e0fd81c06c7931e0ac6a62a17018b3
JesusHolyBalls <sumsar272821@gmail.com>
2022-12-08 03:20:16 +0100

    !important; Yeeeah jump function and advanced movement, gravity is screwed for character
    .grounded is bs, need so much information and the troubleshooting is insanly annoying.
    
    1. need to have the character dimmensions, to use raycasts.(raycats gives a true or false value after detecting if an object is a certain distance from a certain point.) Done
    
    2.right now falling at constant speed which needs to be fixed, it is possible to kinda bhop tho. NOT DONE
    
    3.To fix speed we need to calculate theoretical max speed and replace current speed with "max speed" Done
    
    4. Apperantly fbx files is packed so needed to unpack to make the eyes and ears move with the head and then repack it. Done
    
    5.At this time we cant walk upwards on smal height differences, just end up launching the character upwards (the more speed the higher). NEED HOTFIX
    
    6. To import scripts and characters is a hell, you need to rebind all scripts to (in our case) the different body parts to make them move, I should've named the bone structure parts better for easier navigation and easier distrubution, will be hard for others collaborators to understand.
    
    took about 5 hours to rewrite the movement script and add a working jump function with a "check if grounded"
    
    total programming time is about 8hours now and another 20 hours 3D moddeling (includes getting familiar with the programs).

--- bolibompacs_unity/Assets/Playermodels/DefaultPlayer/movementscript.cs
+++ bolibompacs_unity/Assets/Playermodels/DefaultPlayer/movementscript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58f3da92239ab5ce7760dc41c3ae5a96045f9caed4d63eacbf871c66da468bcb
-size 1700
+oid sha256:bb176074785a8b982a531840025559bd8e4af655a15131e48a76b3ab2a4c918e
+size 2611
