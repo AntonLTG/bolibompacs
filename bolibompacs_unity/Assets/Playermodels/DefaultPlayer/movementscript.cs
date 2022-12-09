3abd0fba874af9789a546a0aae1e7bb99746a3e9
JesusHolyBalls <sumsar272821@gmail.com>
2022-12-09 17:23:39 +0100

    movement is better but slope is still not fixed

--- bolibompacs_unity/Assets/Playermodels/DefaultPlayer/movementscript.cs
+++ bolibompacs_unity/Assets/Playermodels/DefaultPlayer/movementscript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8316b1076126d01f4e9459e7bd5a33d7e42386d9a09ca9a97ff2d9f6116d140c
-size 4696
+oid sha256:32770b5803ef37cf925333d4b153fb71e787d7babf222ceafe5f28de2b8accad
+size 5207
--- bolibompacs_unity/Assets/guns/shootingscript.cs
+++ bolibompacs_unity/Assets/guns/shootingscript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:085d06e8083bab1b8e453049e0ca624b6a07d5a9d7de5cbe1525852e70e7157e
-size 2993
+oid sha256:7b61ee7c3d2480abd992e9bb97227ebc04abe29c368cb13fa465fa208d941a64
+size 2995
